d85d08e603ac7c9b9cd918894ac82b5ab8aa4a83
Tobias Wulvik <tobias.wulvik@gmail.com>
2023-03-20 13:01:07 +0100

    inital setup

---
+++ pages/kurs/[courseSlug]/[topicSlug]/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+interface TopicOverviewPageProps {
+  topics: any[];
+  questions: any[];
+}
+
+const TopicOverviewPage: React.FC<TopicOverviewPageProps> = ({}) => {
+  return <div></div>;
+};
+
+export default TopicOverviewPage;
---
+++ pages/kurs/[courseSlug]/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface CourseOverviewPageProps {}
+
+const CourseOverviewPage: React.FC<CourseOverviewPageProps> = ({}) => {
+  return <div></div>;
+};
+
+export default CourseOverviewPage;
